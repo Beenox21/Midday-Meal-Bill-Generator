33e49463ca1ac40759b676a5dfb460189d453b8d
Deep Murari <99939023+Beenox21@users.noreply.github.com>
2024-06-20 22:48:53 +0530

    first commit

---
+++ src/app/bill/page.tsx
@@ -0,0 +1,159 @@
+'use client'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { useSearchParams } from "next/navigation";
+
+function Bill() {
+    const seachParams = useSearchParams()
+    const budget = seachParams.get('budget')
+    const students = seachParams.get('students')
+
+    let items = [
+        {
+            qty: 0,
+            name: 'Potato',
+            rate: 25,
+            total: 0,
+            individualRate: 0.52
+        },
+        {
+            qty: 0,
+            name: 'Onion',
+            rate: 40,
+            total: 0,
+            individualRate: 0.28
+        },
+        {
+            qty: 0,
+            name: 'Salt',
+            rate: 15,
+            total: 0,
+            individualRate: 0.16
+        },
+        {
+            qty: 0,
+            name: 'Dal',
+            rate: 90,
+            total: 0,
+            individualRate: 0.52
+        },
+        {
+            qty: 0,
+            name: 'Oil',
+            rate: 130,
+            total: 0,
+            individualRate: 0.34
+        },
+        {
+            qty: 0,
+            name: 'Nutrela',
+            rate: 100,
+            total: 0,
+            individualRate: 0.29
+        },
+        {
+            qty: 0,
+            name: 'Peas',
+            rate: 80,
+            total: 0,
+            individualRate: 0.23
+        },
+        {
+            qty: 0,
+            name: 'Chilly',
+            rate: 260,
+            total: 0,
+            individualRate: 0.16
+        },
+        {
+            qty: 0,
+            name: 'Egg',
+            rate: 8,
+            total: 0,
+            individualRate: 0.90
+        },
+        {
+            qty: 0,
+            name: 'Haldi',
+            rate: 300,
+            total: 0,
+            individualRate: 0.17
+        },
+        {
+            qty: 0,
+            name: 'Masala',
+            rate: 330,
+            total: 0,
+            individualRate: 0.20
+        },
+        {
+            qty: 0,
+            name: 'Pooran',
+            rate: 380,
+            total: 0,
+            individualRate: 0.14
+        },
+        {
+            qty: 0,
+            name: 'Firewood',
+            rate: 2000,
+            total: 0,
+            individualRate: 0.77
+        },
+    ]
+
+    let total = 0
+
+    items.map((item) => {
+        item.qty = Math.floor((81 * 85.54 * item.individualRate) / item.rate)
+        item.total = item.qty * item.rate
+        total += item.total
+    }
+    )
+    
+    const veg = budget - total
+
+
+    return (
+        <div>
+            <h1 className="p-4 text-lg font-semibold">Number of students: {students}</h1>
+            <Table>
+    
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px] ">Quantity</TableHead>
+                        <TableHead>Item name</TableHead>
+                        <TableHead>Rate</TableHead>
+                        <TableHead className="text-right">Amount</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {items.map((item) => (
+                        <TableRow>
+                            <TableCell className="font-medium">{item.qty}</TableCell>
+                            <TableCell>{item.name}</TableCell>
+                            <TableCell>{item.rate}</TableCell>
+                            <TableCell className="text-right">{item.total}</TableCell>
+                        </TableRow>
+                    ))}
+
+                    <TableRow>
+                        <TableCell className="">-</TableCell>
+                        <TableCell>Veg</TableCell>
+                        <TableCell>-</TableCell>
+                        <TableCell className="text-right">{veg}</TableCell>
+                    </TableRow>
+
+                    <TableRow>
+                        <TableCell className="font-bold text-lg">Total</TableCell>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                        <TableCell className="text-right font-bold text-lg">{budget}</TableCell>
+                    </TableRow>
+
+                </TableBody>
+            </Table>
+        </div>
+    )
+}
+
+export default Bill
