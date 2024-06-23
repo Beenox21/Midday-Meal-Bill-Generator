008807110b0c07aca32c4974c208d26c1d251a88
Deep Murari <99939023+Beenox21@users.noreply.github.com>
2024-06-23 13:56:52 +0530

    changed designs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
   let [numberOfDays, setNumberOfDays] = useState(113)
   
   return (
-    <div className="flex p-10">
-      <div className="w-full border-2 p-4 rounded-lg bg-slate-100 bg-opacity-40 space-y-5">
+    <div className="flex md:p-10 h-[100vh] bg-slate-100">
+      <div className="w-full md:border-2 p-4 md:rounded-lg  bg-opacity-40 space-y-5">
 
         <div className="space-y-1">
           <h1 className="font-semibold text-lg">Budget for Primary Section</h1>
