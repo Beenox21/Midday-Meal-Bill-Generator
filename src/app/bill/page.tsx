cffa758c912252be90c305379e2283496c3c94c9
Deep Murari <99939023+Beenox21@users.noreply.github.com>
2024-06-23 13:48:55 +0530

    added upper primary

--- src/app/bill/page.tsx
+++ src/app/bill/page.tsx
@@ -1,129 +1,192 @@
 'use client'
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableBody,  TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 
 function Bill() {
-    const seachParams = useSearchParams()
-    let [budget,setBudget] : any = useState(seachParams.get('budget'))
-    let [students,setStudents] : any = useState(seachParams.get('students'))
-    let [veg, setVeg] = useState(0)
+    const searchParams = useSearchParams()
+    const budget : any = {
+        primary : searchParams.get('budgetPrimary'),
+        upperPrimary : searchParams.get('budgetUpperPrimary')
+    }
 
-    let [items, setItems] = useState([
+    const students : any = {
+        primary : searchParams.get('studentsPrimary'),
+        upperPrimary : searchParams.get('studentsUpperPrimary')
+    }
+
+    let items : any= [
         {
-            qty: 0,
+            qtyPrimary : 0,
+            qtyUPrimary : 0,
             name: 'Potato',
             rate: 25,
-            total: 0,
-            individualRate: 0.52
+            totalPrimary: 0,
+            totalUpperPrimary : 0,
+            individualRatePrimary: 0.52,
+            individualRateUpperPrimary : 0.90
         },
         {
-            qty: 0,
+            qtyPrimary : 0,
+            qtyUPrimary : 0,
             name: 'Onion',
             rate: 40,
-            total: 0,
-            individualRate: 0.28
+            totalPrimary: 0,
+            totalUpperPrimary : 0,
+            individualRatePrimary: 0.28,
+            individualRateUpperPrimary : 0.70
         },
         {
-            qty: 0,
+            qtyPrimary : 0,
+            qtyUPrimary : 0,
             name: 'Salt',
             rate: 15,
-            total: 0,
-            individualRate: 0.16
+            totalPrimary: 0,
+            totalUpperPrimary : 0,
+            individualRatePrimary: 0.16,
+            individualRateUpperPrimary : 0.22
         },
         {
-            qty: 0,
+            qtyPrimary : 0,
+            qtyUPrimary : 0,
             name: 'Dal',
             rate: 90,
-            total: 0,
-            individualRate: 0.52
+            totalPrimary: 0,
+            totalUpperPrimary : 0,
+            individualRatePrimary: 0.52,
+            individualRateUpperPrimary : 0.99
         },
         {
-            qty: 0,
+            qtyPrimary : 0,
+            qtyUPrimary : 0,
             name: 'Oil',
             rate: 130,
-            total: 0,
-            individualRate: 0.34
+            totalPrimary: 0,
+            totalUpperPrimary : 0,
+            individualRatePrimary: 0.34,
+            individualRateUpperPrimary : 0.65
         },
         {
-            qty: 0,
+            qtyPrimary : 0,
+            qtyUPrimary : 0,
             name: 'Nutrela',
             rate: 100,
-            total: 0,
-            individualRate: 0.29
+            totalPrimary: 0,
+            totalUpperPrimary : 0,
+            individualRatePrimary: 0.29,
+            individualRateUpperPrimary : 0.55
         },
         {
-            qty: 0,
+            qtyPrimary : 0,
+            qtyUPrimary : 0,
             name: 'Peas',
             rate: 80,
-            total: 0,
-            individualRate: 0.23
+            totalPrimary: 0,
+            totalUpperPrimary : 0,
+            individualRatePrimary: 0.23,
+            individualRateUpperPrimary : 0.40
         },
         {
-            qty: 0,
+            qtyPrimary : 0,
+            qtyUPrimary : 0,
             name: 'Chilly',
             rate: 260,
-            total: 0,
-            individualRate: 0.16
+            totalPrimary: 0,
+            totalUpperPrimary : 0,
+            individualRatePrimary: 0.16,
+            individualRateUpperPrimary : 0.25
         },
         {
-            qty: 0,
+            qtyPrimary : 0,
+            qtyUPrimary : 0,
             name: 'Egg',
             rate: 8,
-            total: 0,
-            individualRate: 0.90
+            totalPrimary: 0,
+            totalUpperPrimary : 0,
+            individualRatePrimary: 0.90,
+            individualRateUpperPrimary : 0.95
         },
         {
-            qty: 0,
+            qtyPrimary : 0,
+            qtyUPrimary : 0,
             name: 'Haldi',
             rate: 300,
-            total: 0,
-            individualRate: 0.17
+            totalPrimary: 0,
+            totalUpperPrimary : 0,
+            individualRatePrimary: 0.17,
+            individualRateUpperPrimary : 0.27
         },
         {
-            qty: 0,
+            qtyPrimary : 0,
+            qtyUPrimary : 0,
             name: 'Masala',
             rate: 330,
-            total: 0,
-            individualRate: 0.20
+            totalPrimary: 0,
+            totalUpperPrimary : 0,
+            individualRatePrimary: 0.20,
+            individualRateUpperPrimary : 0.37
         },
         {
-            qty: 0,
+            qtyPrimary : 0,
+            qtyUPrimary : 0,
             name: 'Pooran',
             rate: 380,
-            total: 0,
-            individualRate: 0.14
+            totalPrimary: 0,
+            totalUpperPrimary : 0,
+            individualRatePrimary: 0.14,
+            individualRateUpperPrimary : 0.10
         },
         {
-            qty: 0,
+            qtyPrimary : 0,
+            qtyUPrimary : 0,
             name: 'Firewood',
             rate: 2000,
-            total: 0,
-            individualRate: 0.77
+            totalPrimary: 0,
+            totalUpperPrimary : 0,
+            individualRatePrimary: 0.77,
+            individualRateUpperPrimary : 0.87
         },
-    ])
+    ]
 
+    const numberOfDays : any = searchParams.get('days')
+    
+    let totalPrimary = 0
+    let totalUpperPrimary = 0
 
-    let total = 0
-    items.map((item) => {
-        item.qty = Math.floor((students * 85.54 * item.individualRate) / item.rate)
-        item.total = item.qty * item.rate
-        total += item.total
-    }
+    items.map((item : any) => {
+        item.qtyPrimary = Math.floor((students.primary * numberOfDays * item.individualRatePrimary) / item.rate)
+        item.totalPrimary = item.qtyPrimary * item.rate
+        totalPrimary += item.totalPrimary
+
+        item.qtyUPrimary = Math.floor((students.upperPrimary * numberOfDays * item.individualRateUpperPrimary) / item.rate)
+        item.totalUpperPrimary = item.qtyUPrimary * item.rate
+        totalUpperPrimary += item.totalUpperPrimary
+     }
     )
+
+    
+    let [veg, setVeg] = useState({
+        primary : 0,
+        upperPrimary : 0
+    })
     
     useEffect(() => {
-        setVeg(budget-total)
+        setVeg({
+            primary : budget.primary-totalPrimary,
+            upperPrimary : budget.upperPrimary-totalUpperPrimary,
+        })
 
     }, [budget])
+
     return (
 
-        <div>
-            <h1 className="p-4 text-lg font-semibold">Number of students: {students}</h1>
-            <h1 className="px-4 text-lg font-semibold">Number of days: 85.54</h1>
-            <h1 className="px-4 text-lg font-semibold">School: Primary</h1>
-            <Table>
+        <div className="mt-3"> 
+            <h1 className="text-2xl text-slate-700 font-bold text-center py-2">MIDDAY MEAL 2023-2024</h1>
+            <h1 className="px-4 text-lg text-slate-600 font-bold">Primary Section</h1>
+            <h1 className="px-4 text-slate-500 ">Number of students: {students.primary}</h1>
+            <h1 className="px-4 text-slate-500 ">Number of days: {numberOfDays}</h1>
+            <Table className="border-t-2 mt-5">
 
                 <TableHeader>
                     <TableRow>
@@ -134,12 +197,12 @@ function Bill() {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {items.map((item, index) => (
+                    {items.map((item : any, index : any) => (
                         <TableRow key={index}>
-                            <TableCell className="font-medium">{item.qty}</TableCell>
+                            <TableCell className="font-medium">{item.qtyPrimary}</TableCell>
                             <TableCell>{item.name}</TableCell>
                             <TableCell>{item.rate}</TableCell>
-                            <TableCell className="text-right">{item.total}</TableCell>
+                            <TableCell className="text-right">{item.totalPrimary}</TableCell>
                         </TableRow>
                     ))}
 
@@ -147,14 +210,55 @@ function Bill() {
                         <TableCell className="">-</TableCell>
                         <TableCell>Veg</TableCell>
                         <TableCell>-</TableCell>
-                        <TableCell className="text-right">{veg}</TableCell>
+                        <TableCell className="text-right">{veg.primary}</TableCell>
                     </TableRow>
 
                     <TableRow>
                         <TableCell className="font-bold text-lg">Total</TableCell>
                         <TableCell></TableCell>
                         <TableCell></TableCell>
-                        <TableCell className="text-right font-bold text-lg">{budget}</TableCell>
+                        <TableCell className="text-right font-bold text-lg">{budget.primary}</TableCell>
+                    </TableRow>
+
+                </TableBody>
+            </Table>
+
+
+            <h1 className="px-4  font-bold text-slate-600 text-lg  mt-3  border-t-2 pt-5">Upper Primary Section</h1>
+            <h1 className="px-4 text-slate-500 ">Number of students: {students.upperPrimary}</h1>
+            <h1 className="px-4 text-slate-500 ">Number of days: {numberOfDays}</h1>
+            <Table className="border-t-2 mt-5">
+
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px] ">Quantity</TableHead>
+                        <TableHead>Item name</TableHead>
+                        <TableHead>Rate</TableHead>
+                        <TableHead className="text-right">Amount</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {items.map((item : any, index : any) => (
+                        <TableRow key={index}>
+                            <TableCell className="font-medium">{item.qtyUPrimary}</TableCell>
+                            <TableCell>{item.name}</TableCell>
+                            <TableCell>{item.rate}</TableCell>
+                            <TableCell className="text-right">{item.totalUpperPrimary}</TableCell>
+                        </TableRow>
+                    ))}
+
+                    <TableRow>
+                        <TableCell className="">-</TableCell>
+                        <TableCell>Veg</TableCell>
+                        <TableCell>-</TableCell>
+                        <TableCell className="text-right">{veg.upperPrimary}</TableCell>
+                    </TableRow>
+
+                    <TableRow>
+                        <TableCell className="font-bold text-lg">Total</TableCell>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                        <TableCell className="text-right font-bold text-lg">{budget.upperPrimary}</TableCell>
                     </TableRow>
 
                 </TableBody>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,28 +5,57 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function Home() {
-  let [students,setStudents] = useState(0)
-  let [budget,setBudget] = useState(0)
 
+  let [students,setStudents] = useState({
+    primary : 0,
+    upperPrimary : 0
+  })
+
+  let [budget,setBudget] = useState(
+    {
+      primary : 0,
+      upperPrimary : 0
+    }
+  )
+
+  let [numberOfDays, setNumberOfDays] = useState(113)
+  
   return (
     <div className="flex p-10">
       <div className="w-full border-2 p-4 rounded-lg bg-slate-100 bg-opacity-40 space-y-5">
 
         <div className="space-y-1">
-          <h1 className="font-semibold text-lg">Budget</h1>
+          <h1 className="font-semibold text-lg">Budget for Primary Section</h1>
           <Input type="number" className="" placeholder="budget amount" 
-          onChange={(e) => setBudget(parseInt(e.target.value))}/>
+          onChange={(e) => setBudget((budget) => ({...budget, primary: parseInt(e.target.value)}))}/>
         </div>
 
         <div className="space-y-1">
-          <h1 className="font-semibold text-lg">Number of students</h1>
+          <h1 className="font-semibold text-lg">Number of students in Primary section</h1>
           <Input type="number" className="" placeholder="no. of students" 
-          onChange={(e) => setStudents(parseInt(e.target.value))}/>
+          onChange={(e) => setStudents((preStudents) => ({...preStudents, primary : parseInt(e.target.value)}))}/>
+        </div>
+
+        <div className="space-y-1">
+          <h1 className="font-semibold text-lg">Budget for Upper Primary Section</h1>
+          <Input type="number" className="" placeholder="budget amount" 
+          onChange={(e) => setBudget((budget) => ({...budget, upperPrimary: parseInt(e.target.value)}))}/>
+        </div>
+
+        <div className="space-y-1">
+          <h1 className="font-semibold text-lg">Number of students in Upper Primary section</h1>
+          <Input type="number" className="" placeholder="no. of students" 
+          onChange={(e) => setStudents((preStudents) => ({...preStudents, upperPrimary : parseInt(e.target.value)}))}/>
+        </div>
+
+        <div className="space-y-1">
+          <h1 className="font-semibold text-lg">Number of days</h1>
+          <Input type="number" className="" placeholder="no. of days" defaultValue={numberOfDays} 
+          onChange={(e) => setNumberOfDays(parseInt(e.target.value))}/>
         </div>
 
         <div>
-
-          <Link href={`/bill?budget=${budget}&students=${students}`}>
+          <Link href={`/bill?budgetPrimary=${budget.primary}&studentsPrimary=${students.primary}&budgetUpperPrimary=${budget.upperPrimary}&studentsUpperPrimary=${students.upperPrimary}&days=${numberOfDays}`}>
             <Button>Generate</Button>
           </Link>
         </div>
